945a4d58ff7708d7a81a5721d07109c91508badc
r1pu5u <rioputrasuryana80@gmail.com>
2026-06-21 06:27:44 +0700

    add SEO & google verification

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://r1pu5u.github.io',
-  integrations: [mdx()]
+  integrations: [mdx(), sitemap()]
 });
